87d754e9438a75ba8bf78189a379ef9b18cd3640
thenftdao <99777666+TheNFTDAO@users.noreply.github.com>
2023-12-02 18:50:14 -0800

    1

--- storagefactory.sol
+++ storagefactory.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import "./simpleStorage.sol";
@@ -11,5 +10,16 @@ contract storageFactory{
         simpleStorage newSimpleStorageContract = new simpleStorage();
 		listOfSimpleStorageContracts.push(newSimpleStorageContract);
 	}
+	function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+	// Address
+	// ABI
+	simpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex]; //what this line says is in the simpleStorage contract, let the mySimpleStorage function get the array of simple storage contracts generated in the listOfSimpleStorageContracts. the array _simpleStorageIndex is being wrapped in the SimpleStorage contract. Now we can call the store function directly from this contract with the following:
+	mySimpleStorage.store(_NewSimpleStorageNumber);
+	}
+	//now we need a function that will let us get(read) data from the new variable mySimpleStorage that we just created. we can get it with the following function. 
+	function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+	SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+	return mySimpleStorage.retrieve();
+}
 
 }
