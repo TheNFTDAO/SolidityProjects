719a80413f62887cda3c8716d87cce9c9053c5f2
NFDAO <thenftdao@gmail.com>
2023-12-07 21:56:19 -0600

    chapter 4 begin

---
+++ fundMe.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract fundMe {
+
+function fund() public payable{
+//allow users to send money
+//require a minimum dollar amount
+
+}
+
+function withdraw() public {
+
+}
+
+}
