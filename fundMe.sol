ae02b9d4087d79b6f4f5bbbffb4ee97c88aec89a
TheNFTDAO <99777666+TheNFTDAO@users.noreply.github.com>
2023-12-08 14:46:23 -0600

    cont

--- fundMe.sol
+++ fundMe.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.18;
 
 contract fundMe {
-
+uint256 public myValue = 1;
 function fund() public payable{
 //allow users to send money
 //require a minimum dollar amount
-require(msg.value > 1e18, didn't send enough eth);
+myValue = myValue + 2;
+require(msg.value > 1e18, "didn't send enough eth");
 }
 
 function withdraw() public {
