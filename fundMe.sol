d8e9d982b366681165d80c08f51e0f654b1e51e2
TheNFTDAO <99777666+TheNFTDAO@users.noreply.github.com>
2023-12-12 12:37:51 -0600

    fundMe array of users and actual value checks before deposit.

--- fundMe.sol
+++ fundMe.sol
@@ -5,12 +5,15 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 
 contract fundMe {
 uint256 public myValue = 1;
-uint8 minimumUsd = 5;
+uint256 public minimumUsd = 5e18;
+address[] public funders;
+mapping(address => uint256) public addressToAmountFunded; // associate the address of the message sender from addressToAmountFunded to a number that lets us identify which number sender it was.
 function fund() public payable{
 //allow users to send money
 //require a minimum dollar amount
 myValue = myValue + 2;
-require(msg.value >= minimumUsd, "didn't send enough eth");
+require(getConversionRate(msg.value) >= minimumUsd, "Up your money son.");
+funders.push(msg.sender); // when the above executes, push the value associated with the funders variable (which would be the address that sends the funded eth, the msg.sender) into the funders array
  }
 function getPrice() public view returns(uint256){
 	// Eth USD price contract Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
@@ -20,7 +23,8 @@ function getPrice() public view returns(uint256){
  (, int price,,,) = priceFeed.latestRoundData(); //the price variable represents the price of eth in usd. We can omit the variables we don't need, but must keep the commas from them. In doing this, we are only assigning return value to the price variable, and the other variables are not assigned.
 return uint256(price * 1e10); // (price times 1 with 10 zeros.)
  }
-function getConversionRate(uint256 ethAmount) public view returns(uint256){// the function getConversionRate will take a uint256 ethAmount as input, and it's results will be returned publicly as a uint256 (result being $ price of eth) 
+function getConversionRate(uint256 ethAmount) public view returns(uint256)
+{// the function getConversionRate will take a uint256 ethAmount as input, and it's results will be returned publicly as a uint256 (result being $ price of eth) 
 
  uint256 ethPrice = getPrice();
  uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // To find the ethAmountInUsd we need to take the price of a whole eth from the chainlink data feed, multiply it by the amount of eth the user sent to the smart contract, and divide it by 1*10^18 because the value returned will have 18 decimal places and we want to eliminate those for a clean answer.
