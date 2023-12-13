41d4da801a656e47e9d4c7409562666f1b90048d
thenftdao <99777666+TheNFTDAO@users.noreply.github.com>
2023-12-12 20:41:37 -0600

    Continued lesson
    learned about send, transfer and call. Learned how call can call functions from other contracts, works like it's own whole transaction. Learned how to set the constructor variable so contracts can know who deployed a contract, and mapped it to the owner variable so the fundMe contract can only have the funds from within it withdrawn by the owner of the contract.

--- fundMe.sol
+++ fundMe.sol
@@ -7,7 +7,11 @@ contract fundMe {
 using priceConverter for uint256;
 uint256 public myValue = 1;
 uint256 public minimumUsd = 5e18;
+address public owner;
 address[] public funders;
+constructor() {
+owner = msg.sender;
+}
 mapping(address => uint256) public addressToAmountFunded; // associate the address of the message sender from addressToAmountFunded to a number that lets us identify which number sender it was.
 function fund() public payable{
 //allow users to send money
@@ -23,15 +27,20 @@ addressToAmountFunded[msg.sender] += msg.value;
  }
 
 function withdraw() public {
-for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++;){
+for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
 //inside the curly braces, we are saying "execute everything inside these curly braces for the duration specified in the for loop.
 //the final part of the for loop (the increments), means that every time we execute the logic within the curly brackets, we are going to add 1 to the funderIndex and make the next increment whatever the logic in the increment section of the for loop is, in this case whatever the current funderIndex is plus 1, until it reaches the full length of the funders array.
 //to access the 0th element of the funders array, we use the following logic:
- funders[fundersIndex]; //this will return an address, since it's an array of addresses
+ funders[funderIndex]; //this will return an address, since it's an array of addresses
  address funder = funders[funderIndex]; //we use this line to associate elements in the array back to the addresses associated with those indexes, and in the next line once the logic to find the address from a particular funder is found, we use the following logic to reset that addresses deposits to 0.
  addressToAmountFunded[funder] = 0;
+ //Next we need to reset the array and withdraw the funds.
+ 
 }
-
+funders = new address[](0);
+//this means the funders variable will be deployed to a new contract with a new array called address with a starting index of 0;
+(bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+require(callSuccess, "Call failed");
  }
 
 }
